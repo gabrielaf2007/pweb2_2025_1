6a827f34acaf970f61678abb3e1bedd6bd3578d6
gabriela <gabriela.f2007@aluno.ifsc.edu.br>
2025-03-17 17:28:33 -0300

    rotas e blade

---
+++ resources/views/aluno/form.blade.php
@@ -0,0 +1,22 @@
+@extends('base')
+@section('titulo','Formulario aluno')
+@section('conteudo')
+
+    <h3>Formulário aluno</h3>
+
+    <table>
+        <thead>
+            <tr>
+                <td>ID</td>
+                <td>Nome</td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Teste</td>
+            </tr>
+        </tbody>
+    </table>
+
+@stop
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,12 @@
 <?php
 
+use App\Http\Controllers\AlunoController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/aluno', [AlunoController::class, 'index'])->name('aluno.index');
+
+Route::get('/aluno/create', [AlunoController::class, 'create'])->name('aluno.index');
